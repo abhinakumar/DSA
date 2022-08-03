7941a532e15fd01bfbbadf34ffea6d0dc47d272c
abhinav kumar <60886857+abhinakumar@users.noreply.github.com>
2022-08-03 21:15:39 +0530

    Time: 7 ms (17.20%), Space: 7 MB (73.67%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,11 +2,47 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        for(int i=0 ; i<n ; i++)                                
-            for(int j=0 ; j<i ; j++)
-                swap(matrix[i][j],matrix[j][i]);
-        
-        for(int i=0 ; i<n ; i++)                                        
-            reverse(matrix[i].begin(),matrix[i].end());
-    }
+       int sr=0;
+    int sc=0;
+    int er=n-1;
+    int ec=n-1;
+    
+    while (sr<er && sc<ec)
+    {
+        for(int i=1; i<n;i++)
+        {
+            int temp1=matrix[sr][sc];
+            int temp2;
+            for(int j=sc ; j<ec ; j++)
+            {
+                temp2=matrix[sr][j+1];
+                matrix[sr][j+1]=temp1;
+                temp1=temp2;
+            }
+            for(int j=sr;j<er;j++)
+            {
+                temp2=matrix[j+1][ec];
+                matrix[j+1][ec]=temp1;
+                temp1=temp2;
+            }
+            for(int j=ec;j>sc ; j--)
+            {
+                temp2=matrix[er][j-1];
+                matrix[er][j-1]=temp1;
+                temp1=temp2;
+            }
+            for(int j=er ; j>sr ;j--)
+            {
+                temp2=matrix[j-1][sc];
+                matrix[j-1][sc]=temp1;
+                temp1=temp2;
+            }
+            
+        }
+        sr++;
+        sc++;
+        er--;
+        ec--;
+        n-=2;
+    }}
 };
