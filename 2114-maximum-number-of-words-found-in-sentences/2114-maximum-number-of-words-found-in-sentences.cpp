71a526db0e34bc901c627efabb73654fd8ce66de
abhinav kumar <60886857+abhinakumar@users.noreply.github.com>
2022-08-17 16:53:13 +0530

    Time: 42 ms (5.47%), Space: 10.5 MB (7.99%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int mostWordsFound(vector<string>& s) {
-        int ans = -1;
-        for(int i = 0 ; i < s.size() ; i++){
-            int cnt = 1 ;
-            for(int j = 0 ; j < s[i].size() ; j++){
-                if(s[i][j]==' ')cnt++;
-            }
-            ans = max(ans, cnt);
+    int mostWordsFound(vector<string>& sentences) {
+        int maxWords=0;
+        for(int i=0; i<sentences.size(); i++) {
+            stringstream ss(sentences[i]);
+            int count=0;
+            string word;
+            while(ss>>word)
+                count++;
+            if(count>maxWords)
+                maxWords=count;
         }
-        return ans;
+        return maxWords;
     }
 };
