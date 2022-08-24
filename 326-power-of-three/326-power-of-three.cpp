a551448492e222b1247009a18d6cf2d0c1656b68
abhinav kumar <60886857+abhinakumar@users.noreply.github.com>
2022-08-24 19:22:03 +0530

    Time: 24 ms (79.84%), Space: 6 MB (12.98%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+         if(n==0)return false;
+        if(ceil(log10(n)/log10(3))==log10(n)/log10(3)){
+            return true;
+        }
+        return false;
+    }
+};
