5d457961381229b7232ab4d8cdb62aca3e6f1789
abhinav kumar <60886857+abhinakumar@users.noreply.github.com>
2022-08-25 12:22:10 +0530

    Time: 59 ms (9.05%), Space: 8.7 MB (89.81%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canConstruct(string a, string b) {
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());       
+        int j=0;
+        for(auto i : b)
+        {
+            if(i == a[j] && j < a.length()) j++;
+        }
+        return (j>=a.length()); 
+    }
+};
