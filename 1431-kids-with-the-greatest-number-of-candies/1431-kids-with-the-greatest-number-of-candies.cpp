27c432db6bb871607b4b10ef5e8d25892d7b1111
abhinav kumar <60886857+abhinakumar@users.noreply.github.com>
2022-08-27 19:56:54 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (83.75%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        // vector<bool> bo;
-        // int max1=0;
-        // for(auto i:candies)
-        //     max1=max(max1,candies[i]);
-        // for(int i=0;i<candies.size();i++)
-        // {
-        //     if((candies[i]+extraCandies)>=max1)
-        //        bo.push_back(1);
-        //    else
-        //         bo.push_back(0);
-        // }
-        // return bo;
-        vector<bool> out;
-        int max = *max_element(candies.begin(), candies.end());
-        
-        for(int i=0; i < candies.size(); i++){
-            if(candies[i]+extraCandies >= max)
-                out.push_back(true);
-            else
-                out.push_back(false);
+        vector<bool> bo;
+        int max1=0;
+        for(int i=0;i<candies.size();i++)
+            max1=max(max1,candies[i]);
+        for(int i=0;i<candies.size();i++)
+        {
+            if((candies[i]+extraCandies)>=max1)
+               bo.push_back(1);
+           else
+                bo.push_back(0);
         }
-        
-        return out;
+        return bo;
     }
 };
 /*vector<bool> out;
